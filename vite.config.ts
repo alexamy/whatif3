7ebfcfd0339a7d459b4fc605519d459acd25d5af
Aleksandr Chashchin <achasch@gmail.com>
2025-04-15 23:11:10 +0200

    add babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import babel from 'vite-plugin-babel';
 import observerPlugin from './babel-mobx-observer.js';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    babel({
+      babelConfig: {
+        plugins: [observerPlugin],
+      },
+    }),
     tailwindcss(),
   ],
   clearScreen: false,
