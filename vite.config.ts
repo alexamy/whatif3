3ae0eaefc4d57be4fbcfb99a0130b8123adaa74d
Aleksandr Chashchin <ac@happynumbers.com>
2025-04-15 19:32:42 +0200

    remove vite mobx plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import observerPlugin from 'mobx-react-observer/swc-plugin'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      plugins: [observerPlugin() as any],
-    }),
+    react(),
     tailwindcss(),
   ],
   clearScreen: false,
