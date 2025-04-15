e223f4a71f837ff6f04770060995f3a1c56b450f
Aleksandr Chashchin <achasch@gmail.com>
2025-04-16 01:09:15 +0200

    remove observer plugin

--- src/lib/Mobx.res
+++ src/lib/Mobx.res
@@ -2,7 +2,6 @@
 external observable: 'a => 'a = "observable"
 
 @module("mobx-react-lite")
-/** You do not need to use this function, it is automatically added by babel-mobx-observer script. */
 external observer: React.component<'a> => React.component<'a> = "observer"
 
 @module("mobx")
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import babel from 'vite-plugin-babel';
-import observerPlugin from './babel-mobx-observer.js';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    babel({
-      filter: /\.res\.mjs$/,
-      exclude: /node_modules/,
-      babelConfig: {
-        plugins: [observerPlugin],
-      },
-    }),
     tailwindcss(),
   ],
   clearScreen: false,
